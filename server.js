163b466df2beba80459e4822f7311b38a7c14606
jpkashlak <jpkashlak@gmail.com>
2021-03-12 10:06:43 -0600

    debugged voterRoutes inputCheck

--- server.js
+++ server.js
@@ -1,26 +1,26 @@
-const db = require('./db/database')
 const express = require('express');
+const db = require('./db/database');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
-const inputCheck = require('./utils/inputCheck');
+
 const apiRoutes = require('./routes/apiRoutes');
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 // Use apiRoutes
 app.use('/api', apiRoutes);
 
-//Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-
-// Default (and final) response for any other request (Not Found) Catch all
+// Default response for any other request(Not Found) Catch all other
 app.use((req, res) => {
-    res.status(404).end();
+  res.status(404).end();
 });
 
-// Start server AFTER db connection
+// Start server after DB connection
 db.on('open', () => {
-    app.listen(PORT, () => {
-        console.log(`Server running on port ${PORT}`);
-    });
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
     
